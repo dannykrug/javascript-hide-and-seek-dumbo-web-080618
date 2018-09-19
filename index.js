2100deb06276ba7328bdee5a1efecd9793477a83
dannykrug <dannykrug@gmail.com>
2018-09-19 09:54:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector (selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.getElementById("nested").querySelector(".target");
+}
+
+function increaseRankBy(n) {
+  let numbers = document.getElementById("app").querySelectorAll("ul.ranked-list li")
+  for (let i=0;i<numbers.length;i++) {
+    numbers[i].innerHTML=parseInt(numbers[i].innerHTML)+n;
+  }
+}
+
+function deepestChild() {
+return document.getElementById('grand-node').querySelector("div div div div div")
+}
